ee5c016df0417c0b4d7e46bff63c4939d6a122da
smeno004 <siddharth.krishnaleela@gmail.com>
2017-10-21 13:51:16 -0700

    Outputting list of contents read from input

--- test.py
+++ test.py
@@ -1,27 +1,47 @@
 import analyze as analyze
 import json as json
+import collections
 
 def entities(userIput):
     return
     
 def syntax(userInput):
+    intentMatch = open("./matchTable.json").read()
     analysisJSON = analyze.analyze_syntax(userInput)
+    contentGroup = collections.namedtuple("content", 'tag content')
+
+    print ("Size: ", len(analysisJSON["tokens"]))
+
+    analyzedContent = [0 for i in range(0,len(analysisJSON["tokens"]))]
+
+    j = 0
+
+    for i in analysisJSON["tokens"]:
+        curContentGroup = contentGroup(tag=i["partOfSpeech"]["tag"], content=i["text"]["content"])
+        analyzedContent[j] = curContentGroup
+        j += 1
+
+    
+
+    return analyzedContent
     #print (json.dumps(analysisJSON, indent=2))
 
     #print ("\t", analysisJSON["tokens"][0]["partOfSpeech"]["tag"])
-    return analysisJSON
-    return
+    #return analysisJSON
 
 if __name__ == '__main__':
+
     userInput = input("Enter string:")
 
     if entities(userInput) != []:
-        print("Entities detected! do something userful with them lolol")
+        print("Entities detected! do something useful with them")
         
-    syntax_json = syntax(userInput)
+    synReturn = syntax(userInput)
+
+    print (synReturn)
     
     
-    for i in syntax_json["tokens"]:
-        print (i["partOfSpeech"]["tag"], "\t", i["text"]["content"], "\n")
+    #for i in syntax_json["tokens"]:
+        #print (i["partOfSpeech"]["tag"], "\t", i["text"]["content"], "\n")
     
-    print (userInput)
+    #print (userInput)
