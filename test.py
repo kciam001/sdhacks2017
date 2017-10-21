a52aeb2f45935fdb2143f6ac156b1446c1310cff
Andre-Castro <acast050@ucr.edu>
2017-10-21 12:04:51 -0700

    added entities/syntax function def

--- test.py
+++ test.py
@@ -1,14 +1,27 @@
 import analyze as analyze
 import json as json
 
-if __name__ == '__main__':
-    userinput = input("Enter string:")
-    analysisJSON = analyze.analyze_syntax(userinput)
+def entities(userIput):
+    return
+    
+def syntax(userInput):
+    analysisJSON = analyze.analyze_syntax(userInput)
     #print (json.dumps(analysisJSON, indent=2))
 
     #print ("\t", analysisJSON["tokens"][0]["partOfSpeech"]["tag"])
+    return analysisJSON
+    return
 
-    for i in analysisJSON["tokens"]:
+if __name__ == '__main__':
+    userInput = input("Enter string:")
+
+    if entities(userInput) != []:
+        print("Entities detected! do something userful with them lolol")
+        
+    syntax_json = syntax(userInput)
+    
+    
+    for i in syntax_json["tokens"]:
         print (i["partOfSpeech"]["tag"], "\t", i["text"]["content"], "\n")
     
-    print (userinput)
+    print (userInput)
